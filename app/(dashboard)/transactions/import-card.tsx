85b37a6e877e03f133cc8605458f8c90fa5f3721
LukaD45 <luka.dusko44@gmail.com>
2024-09-10 16:23:06 +0200

    Feat: Continue button disabled prop

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -26,6 +26,28 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
   );
   const headers = data[0];
   const body = data.slice(1);
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
+
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+      if (value === "skip") {
+        value = null;
+      }
+      newSelectedColumns[`column_${columnIndex}`] = value;
+      return newSelectedColumns;
+    });
+  };
+
+  const progress = Object.values(SelectedColumns).filter(Boolean).length;
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -33,9 +55,16 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
           <CardTitle className="text-xl line-clamp-1">
             Import Transaction
           </CardTitle>
-          <div className="flex items-center gap-x-2">
-            <Button onClick={onCancel} size="sm">
+          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
+            <Button onClick={onCancel} size="sm" className="w-full lg:w-auto">
               Cancel
+            </Button>
+            <Button
+              className="w-full lg:w-auto"
+              size="sm"
+              disabled={progress < requiredOptions.length}
+            >
+              Continue ({progress}/{requiredOptions.length})
             </Button>
           </div>
         </CardHeader>
@@ -44,7 +73,7 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
             headers={headers}
             body={body}
             selectedColumns={SelectedColumns}
-            onTableHeadSelectChange={() => {}}
+            onTableHeadSelectChange={onTableHeadSelectChange}
           />
         </CardContent>
       </Card>
