f4f44ff976e03040e2a810d62d901b9012d9a6e5
LukaD45 <luka.dusko44@gmail.com>
2024-09-23 11:57:56 +0200

    Loading state dashbpard charts/pie

--- components/spending-pie.tsx
+++ components/spending-pie.tsx
@@ -7,12 +7,13 @@ import {
 } from "./ui/select";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
-import { FileSearch, PieChart, Radar, Target } from "lucide-react";
+import { FileSearch, Loader2, PieChart, Radar, Target } from "lucide-react";
 import { useState } from "react";
 
-import { AreaVariant } from "./area-variant";
-import { BarVariant } from "./bar-variant";
 import { PieVariant } from "./pie-variant";
+import { RadarVariant } from "./radar-variant";
+import { RadialVariant } from "./radial-variant";
+import { Skeleton } from "./ui/skeleton";
 
 type Props = {
   data?: {
@@ -69,10 +70,26 @@ export const SpendingPie = ({ data = [] }: Props) => {
         ) : (
           <>
             {chartType === "pie" && <PieVariant data={data} />}
-            {chartType === "radar" && <AreaVariant data={data} />}
-            {chartType === "radial" && <BarVariant data={data} />}
+            {chartType === "radar" && <RadarVariant data={data} />}
+            {chartType === "radial" && <RadialVariant data={data} />}
           </>
         )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export const SpendingPieLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-md">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items:center justify-between">
+        <Skeleton className="h-8 w-48" />
+        <Skeleton className="h-8 lg:w-[120px] w-full" />
+      </CardHeader>
+      <CardContent>
+        <div className="h-[350px] w-full flex items-center justify-center">
+          <Loader2 className="size-6 text-slate-300 animate-spin" />
+        </div>
       </CardContent>
     </Card>
   );
